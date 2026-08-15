6a170a270fdebe27bc9297d5c1abd95719a498a2
gadisaka <gadisaka04@gmail.com>
2026-08-15 20:50:13 +0300

    Update API_URL in constants.js for production environment and comment out local URL

--- admin/constants.js
+++ admin/constants.js
@@ -1,4 +1,4 @@
 // Local:
-export const API_URL = "http://localhost:3001/api";
+//export const API_URL = "http://localhost:3001/api";
 // Production:
-// export const API_URL = "https://api.251bet.net/api";
+ export const API_URL = "https://api.251bet.net/api";
